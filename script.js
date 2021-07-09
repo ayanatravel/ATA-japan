1b7345df4e439b6a9578dee1bdd94c2f34755196
ayanatravel <87166718+ayanatravel@users.noreply.github.com>
2021-07-09 20:09:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-const swiper = new Swiper('.swiper-container', {
-  speed: 1000,
-  loop: true,
-  effect: 'fade',
+$(function(){
+    //地域を選択
+    $('.area-btn').click(function(){
+        $('.area-overlay').show();
+        $('.pref-area').show();
+        var area = $(this).data('area');
+        $('[data-list]').hide();
+        $('[data-list="' + area + '"]').show();
+    });
+    
+    //レイヤーをタップ
+    $('.area-overlay').click(function(){
+        prefReset();
+    });
+    
+    //都道府県をクリック
+    $('.pref-list [data-id] ').click(function(){
+        if($(this).data('id')){
+          var  id= $(this).data('id');          
+          //このidを使用して行いたい操作をしてください
+          //都道府県IDに応じて別ページに飛ばしたい場合はこんな風に書く↓
+          window.location.href = 'https://kinocolog.com/pref_select/' + id ;
+          
+          prefReset();
 
-  autoplay: {
-    delay: 2000
-  },
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-
+        }
+    });
+    
+    //表示リセット
+    function prefReset(){
+        $('[data-list]').hide();
+        $('.pref-area').hide();
+        $('.area-overlay').hide();
+    }
 });
+
